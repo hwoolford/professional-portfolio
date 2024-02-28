18a36a8a88f18ec7fa3d26ed12d6b507f414fb3a
forxtoisate <hwoolford@gmail.com>
2024-02-28 13:47:49 -0600

    Added minimal code to the pages to see if I could get them to render to the page

--- professional-portfolio/src/components/pages/Contact.jsx
+++ professional-portfolio/src/components/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { validateEmail } from "../utils/helpers";
+import { validateEmail } from "../../utils/helpers";
 
 export default function Contact() {
   const [name, setName] = useState("");
