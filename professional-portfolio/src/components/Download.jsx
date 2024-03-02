871061bdbd025c8a26a9ddd11c6ea9176e4e2f3e
forxtoisate <hwoolford@gmail.com>
2024-03-02 13:27:05 -0600

    Added a download component and download css for resume and added component to resume component

---
+++ professional-portfolio/src/components/Download.jsx
@@ -0,0 +1,9 @@
+import "../styles/download.css"
+
+export default function Download() {
+    return (
+        <div className="container">
+        <a className="btn" href="./assets/resume2024.pdf" download="hwresume.pdf">Download Resume</a>
+        </div>
+    );
+}
--- professional-portfolio/src/components/pages/Resume.jsx
+++ professional-portfolio/src/components/pages/Resume.jsx
@@ -1,5 +1,10 @@
+import Download from "../Download"
+
 export default function Resume() {
     return (
+        <>
         <h1 className="text-center title">Resume</h1>
+        <Download />
+        </>
         )
 }
