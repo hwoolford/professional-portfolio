4bcd991cd5ee2fbff06f7b9cb1d2b942e93be237
forxtoisate <hwoolford@gmail.com>
2024-03-05 10:28:33 -0600

    fixed social media logos in footer so they change color when hover

--- professional-portfolio/src/components/Footer.jsx
+++ professional-portfolio/src/components/Footer.jsx
@@ -3,21 +3,30 @@ import '../styles/footer.css'
 export default function Footer() {
   return (
     <footer className="footer fixed-bottom">
-      {/* <div className='githubContainer'> */}
+      <div className='socialContainer'>
     <a href="https://github.com/hwoolford" target="_blank" rel="noreferrer">
-      <img className="socialLogo" src="./assets/githublogo.png" alt="GitHub Repo"></img>
+      <img className="socialLogo normal" src="./assets/githublogo.png" alt="GitHub Repo"></img>
     </a>
-    {/* <a href="https://github.com/hwoolford" target="_blank" rel="noreferrer">
-      <img className="socialLogo" src="./assets/githublogowhite.png" alt="GitHub Repo"></img>
-    </a> */}
-    {/* </div> */}
+    <a href="https://github.com/hwoolford" target="_blank" rel="noreferrer">
+      <img className="socialLogo hover" src="./assets/githublogowhite.png" alt="GitHub Repo"></img>
+    </a>
+    </div>
+    <div className='socialContainer'>
     <a href="https://www.linkedin.com/in/hwoolford/" target="_blank" rel="noreferrer">
-    <img className="socialLogo" src="./assets/linkedinlogo.png" alt="LinkedIn Page"></img>
+    <img className="socialLogo normal" src="./assets/linkedinlogo.png" alt="LinkedIn Page"></img>
+  </a>
+  <a href="https://www.linkedin.com/in/hwoolford/" target="_blank" rel="noreferrer">
+    <img className="socialLogo hover" src="./assets/linkedinlogowhite.png" alt="LinkedIn Page"></img>
+  </a>
+  </div>
+  <div className='socialContainer'>
+  <a href="https://stackoverflow.com/users/23531312/forxtoisate" target="_blank" rel="noreferrer">
+    <img className="socialLogo normal" src="./assets/stacklogo.png" alt="Stackoverflow Page"></img>
   </a>
   <a href="https://stackoverflow.com/users/23531312/forxtoisate" target="_blank" rel="noreferrer">
-    <img className="socialLogo" src="./assets/stacklogo.png" alt="Stackoverflow Page"></img>
+    <img className="socialLogo hover" src="./assets/stacklogowhite.png" alt="Stackoverflow Page"></img>
   </a>
-  
+  </div>
   </footer>
   );
 }
